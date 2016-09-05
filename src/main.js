1abee4cc0acfcc9d02f40fe603df26b6e0009169
dante <c.dante.federici@gmail.com>
2016-09-04 22:46:38 -0400

    Working on fixing some base stuff

--- src/main.js
+++ src/main.js
@@ -1,39 +1,4 @@
 import './main.scss';
 
-const loading = document.querySelector('.content-loading');
-loading.classList.add('hidden');
-
-import angular from 'angular';
-import ngRedux from 'ng-redux';
-import ReduxThunk from 'redux-thunk';
-
-const rootReducer = (state = {}, action) => {
-	console.debug('State', state, 'Action', action);
-	return state;
-};
-
-
-const app = angular.module('life-events', [ngRedux])
-	.config(['$ngReduxProvider', ($ngReduxProvider) => {
-		$ngReduxProvider.createStoreWith(
-			rootReducer,
-			[
-				ReduxThunk,
-			]
-		);
-	}]);
-
-
-class EventFormComponent {
-	constructor($ngRedux) {
-		console.debug('Hey, you', $ngRedux.getState());
-	}
-}
-
-
-import eventFormTpl from './eventForm.tpl.jade';
-app.component('eventForm', {
-	template: eventFormTpl,
-	controller: ['$ngRedux', EventFormComponent],
-});
+console.debug('Hello world');
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,14 @@ module.exports = {
 	module:	{
 		loaders: [
 			{ test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-			{ test: /\.(eot|woff|woff2|ttf|svg|png|jpg)(\?.*)?$/, loaders: ['file'] },
+			{ test: /\.(ico|eot|woff|woff2|ttf|svg|png|jpg)(\?.*)?$/, loaders: ['file'] },
 			{ test: /\.css$/, loaders: ['style', 'css', 'postcss'] },
 			{ test: /\.less$/, loaders: ['style', 'css', 'postcss', 'less'] },
 			{ test: /\.tpl\.(pug|jade)$/, loaders: ['html?removeRedundantAttributes=false', 'jade-html'] },
 			{ test: /\.tpl\.html$/, loaders: ['html?removeRedundantAttributes=false'] },
 			{ test: /[^\.][^t][^p][^l]\.(pug|jade)$/, loaders: ['file?name=[name].html', 'jade-html' ] },
-			{ test: /\.(sass|scss)$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+			{ test: /index\.scss$/, loaders: ['file?name=[name].css', 'postcss', 'sass']},
+			{ test: /\.(sass|scss)$/, loaders: ['style', 'css', 'postcss', 'sass'], exclude: /index\.scss$/ },
 			{ test: /[^\.][^t][^p][^l]\.html$/, loaders: ['file?name=[name].[ext]'] }
 		]
 	},
@@ -30,7 +31,14 @@ module.exports = {
 			module: false
 		})
 	],
-	entry: { app: ['./index.jade', './main.js'] },
+	entry: {
+		app: [
+			'./favicon.ico',
+			'./index.scss',
+			'./index.jade',
+			'./main.js',
+		]
+	},
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(process.cwd(), 'bin')
