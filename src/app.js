6a15d7ee60635d558129ccaaddb7e76980a285a4
dante <c.dante.federici@gmail.com>
2017-09-09 16:35:39 -0400

    Base angular implemented.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 import './app.scss';
+import angular from 'angular';
 
-console.debug('Hello world');
+// Declare our application
+const APP_MODULE = 'my-app';
+const app = angular.module(APP_MODULE, [])
+	.config([() => {
+		console.debug('angular.config', 'Configure providers / factories /etc');
+	}])
+	.run([() => {
+		console.debug('angular.run', 'Bootstrap runtime modules like redux or a router');
+	}]);
+
+// Define app components
+import { MainController } from './mainComponent/main';
+import mainTpl from './mainComponent/main.tpl.pug';
+app.component('main', {
+	template: mainTpl,
+	controller: [MainController],
+	controllerAs: '$ctrl',
+});
+
+// When we have the document, bootstrap our application
+angular.element(document).ready(() => {
+	angular.bootstrap(document, [APP_MODULE]);
+});
---
+++ src/mainComponent/main.js
@@ -0,0 +1,5 @@
+export class MainController {
+	constructor() {
+		this.name = 'Topps Belubie';
+	}
+}
