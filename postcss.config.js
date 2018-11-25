aaa56e81c30afdde61abed725a13b51b678b9771
dante <c.dante.federici@gmail.com>
2018-11-25 15:45:41 -0500

    Postcss plugin ordering.

--- postcss.config.js
+++ postcss.config.js
@@ -20,8 +20,11 @@ module.exports = {
 	 * https://github.com/postcss/postcss-url
 	 */
 	plugins: {
-		'postcss-nested': {},
-		'postcss-import': {},
+		// Note on ordering: They claim declaration order, which is awkward for objects...
+		// Order matters in this object!!!
+		// @see: https://github.com/michael-ciniawsky/postcss-load-config#ordering
+		'postcss-import': {}, // import plugin first!!! this is so other plugins are seen after import.
+		'postcss-nested': {}, // nesting next, so our imports are followed.
 		'postcss-preset-env': {},
 	},
 };
